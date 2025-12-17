f3fa17f5f5e2950cfeb72780596b30be5bbb117f
abdel.zaher.amir <abdel.zaher.amir.21k@egressy.info>
2025-12-17 10:38:17 +0100

    Nuget Updates

--- NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
+++ NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7b6163aa436a0ae3826683ad8b365923ed1b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1ab1db072aabf447890e48209ad57d0e553d70")]
 [assembly: System.Reflection.AssemblyProductAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
