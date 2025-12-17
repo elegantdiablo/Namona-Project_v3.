7316f5b87befa6b6475d817eb88de9b86d78e145
abdel.zaher.amir <abdel.zaher.amir.21k@egressy.info>
2025-12-17 09:59:22 +0100

    AllClothes Dto and ClothesModel changes

--- NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
+++ NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff00b8772f1323bc1adc9e9c26bb0f9b8fc01c7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b5b2b69c6f2986f1e4816420a92ee213c1f4d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
