94a76fbb71ab56f18e3c998f85fbac1448c140f2
Abd-El Zaher Amir <mcamir03@gmail.com>
2026-01-08 19:13:39 +0100

    Created GetCartContent, EditCart and DeleteCart function in CartController.cs

--- NamonaProject_v3/NamonaProject_v3/Controllers/CartController.cs
+++ NamonaProject_v3/NamonaProject_v3/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NamonaProject_v3_.DTO;
 using NamonaProject_v3_.Model;
 
 namespace NamonaProject_v3_.Controllers
@@ -13,6 +14,48 @@ namespace NamonaProject_v3_.Controllers
         public CartController(CartModel cartModel)
         {
             _cartModel = cartModel;
+        }
+
+        [HttpGet("/CartContent")]
+        public ActionResult<CartDto> GetCartContent()
+        {
+            try
+            {
+                return Ok(_cartModel.GetCartContent());
+            }
+            catch
+            {
+                return NoContent();
+            }
+        }
+    
+
+        [HttpPut("/EditCart/{id}")]
+        public ActionResult EditCart(int id, CartItemDto dto)
+        {
+            try
+            {
+                _cartModel.EditCart(id, dto);
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("/DeleteCartItem/{id}")]
+        public ActionResult DeleteCartItem(int id)
+        {
+            try
+            {
+                _cartModel.DeleteClothes(id);
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
     }
 }
