0461837d7c4179469f7886f3329999d50f37e53d
KrisszUser <kovacs.kristof20k@egressy.info>
2026-02-16 10:25:48 +0100

    ok

---
+++ NamonaProject_v3/NamonaProjectTest/CartModelTest.cs
@@ -0,0 +1,32 @@
+﻿using NamonaProject_v3_.Model;
+using NamonaProject_v3_.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamonaProjectTest
+{
+    public class CartModelTest
+    {
+        private readonly CartModel _model;
+        private readonly NamonaDbContext _context;
+
+        public CartModelTest()
+        {
+            _context = DbContextFactory.Create();
+            _model = new CartModel(_context);
+        }
+
+        [Fact]
+        public void GetCartData_Valid()
+        {
+            var result = _model.GetCartData().ToList();
+
+            Assert.NotEmpty(result);
+            Assert.All(result, r => Assert.True(r.CartId > 0));
+            Assert.All(result, r => Assert.True(r.UserId > 0));
+        }
+    }
+}
