1b2f40459de81c8f115cfce6b308d790a1a7ad1c
Vagra Szabolcs <varga.szabolcs20k@egressy.info>
2026-03-05 11:56:50 +0100

    idk

---
+++ NamonaAvalonia/NamonaAvalonia/Dto/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamonaAvalonia.Dto
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Role { get; set; }
+    }
+}
