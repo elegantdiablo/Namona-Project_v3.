30b9574195cbedbe8a956c33fa99912d54ace5c1
Vagra Szabolcs <varga.szabolcs20k@egressy.info>
2026-03-05 10:57:53 +0100

    asd

--- NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
+++ NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3af5ba84cb5388eaed23420ec99eed16b0afd80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04fcebed57bc62159cfccd663590aad650df5626")]
 [assembly: System.Reflection.AssemblyProductAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
