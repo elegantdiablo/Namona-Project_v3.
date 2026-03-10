ae1c355a2d9a0470164ffdd0424ee24025f2762d
Abd-El Zaher Amir <mcamir03@gmail.com>
2026-03-10 14:25:24 +0100

    asd

--- NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
+++ NamonaProject_v3/NamonaProject_v3/obj/Debug/net8.0/NamonaProject_v3..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc26ab999191ccdcda7a3b8edd6b3d72cc69a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ddd1344900903243700d18d99e465e4a4e5f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NamonaProject_v3.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
